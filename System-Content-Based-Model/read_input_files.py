e586927bfd3848f7eb6a5499af78eb7bf5dbcb2f
Samuel Martín <alu0101359526@ull.edu.es>
2023-10-27 10:26:22 +0100

    Implemented the function that read the content of the documents of the plain text file

--- System-Content-Based-Model/read_input_files.py
+++ System-Content-Based-Model/read_input_files.py
@@ -14,7 +14,7 @@ import sys
   # @param input_file the input file.
   # @return The different lines of the input file.
 # 
-def read_input_file(input_file):
+def read_input_lines_files(input_file):
   # To start with, we check if the input file exists and if it is a file.
   if os.path.exists(input_file) and os.path.isfile(input_file):
     extension = os.path.splitext(input_file)[1]
@@ -26,7 +26,7 @@ def read_input_file(input_file):
         with open(input_file, 'r') as file: # Open the file in read mode.
           rows = []
           for line in file:
-            rows.append(line.strip().split())
+            rows.append(line)
           # print()
           # print("The content of the file is the following:")
           # for line in rows:
@@ -39,3 +39,29 @@ def read_input_file(input_file):
         sys.exit(1)
         
       return rows
+
+def read_input_files(input_file):
+  # To start with, we check if the input file exists and if it is a file.
+  if os.path.exists(input_file) and os.path.isfile(input_file):
+    extension = os.path.splitext(input_file)[1]
+    if extension != ".txt":
+      print("The input file must be a `.txt` file.")
+      sys.exit(1) # Exit with error type 1.
+    else:
+      try:
+        
+        with open(input_file, 'r') as file: # Open the file in read mode.
+          string = ""
+          for line in file:
+            string += line.replace("\n", "")
+          #print()
+          #print("The content of the all file is the following:")
+          #print(string)
+      except FileNotFoundError:
+        print(f"The file {input_file} doesn't exist.")
+        sys.exit(1)
+      except Exception as e:
+        print(f"An error occurred while reading the file: {str(e)}")
+        sys.exit(1)
+        
+      return string
--- System-Content-Based-Model/recommenadation_system.py
+++ System-Content-Based-Model/recommenadation_system.py
@@ -10,7 +10,8 @@ import os
 import sys
 import numpy as np
 
-from read_input_file_system import read_input_file
+from read_input_files import read_input_files
+from read_input_files import read_input_lines_files
 
 def recommendation_system(plain_text_file, stop_word_file, lematization_of_terms_file):
   print("Welcome to the recommendation system!")
@@ -20,6 +21,28 @@ def recommendation_system(plain_text_file, stop_word_file, lematization_of_terms
   print("Stop Word file: " + str(stop_word_file))
   print("Lematization of terms file " + str(lematization_of_terms_file))
   
-  plain_text_data = read_input_file(plain_text_file)
-  stop_word_data = read_input_file(stop_word_file)
-  lematization_of_terms_data = read_input_file(lematization_of_terms_file)
+  # Lectura de las palabras de parada
+  stop_word_data = read_input_lines_files(stop_word_file)
+  
+  # Lectura de los ficheros de documentos que se encuentran dentro de el fichero plain_text_file.
+  plain_text_data = read_input_lines_files(plain_text_file)
+  documents = []
+  for line in plain_text_data:
+    path = "../Data/Documents/" + line.replace("\n", "")
+    documents.append(read_input_files(str(path)))
+  
+  # lematization_of_terms_data = read_input_file(lematization_of_terms_file)
+  
+  # At this point starts the implementation of the content based model.
+  
+  # # First of all, we need to obtain the different terms of the plain text file.
+  # terms = []
+  # for line in plain_text_data:
+  #   for term in line:
+  #     if term not in terms:
+  #       terms.append(term)
+  
+  # # Comprobación de que se han obtenido bien los términos.
+  # print()
+  # print("The different terms of the plain text file are the following:")
+  # print(terms)
